9d5f743a2535ae988375ab1db414be71b0d39ee2
Aniket Singh <72395988+anisirji@users.noreply.github.com>
2022-07-04 13:28:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const qno = document.getElementById('Qno')
+const question = document.getElementById("question")
+const options = document.getElementById('options')
+const button = document.getElementById('button')
+
