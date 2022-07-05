5c65d6da13131119da775228ec21b08029941ece
anisirji <www.allinoneforyouhere@gmail.com>
2022-07-05 09:09:43 +0530

    looping through object:

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 'use strict'
 
+
 const qno = document.getElementById('Qno')
 const question = document.getElementById("question")
 const options = document.getElementById('options')
 const button = document.getElementById('button')
 
+const questionJson = {
+    1:{
+        "Q":"MS-Word is an example of _____",
+        "option":["An operating system","A processing device","Application software","An input device"],
+        "ans":"Application software"
+    },
+    2:{
+        "Q":"Ctrl, Shift and Alt are called .......... keys.",
+        "option":["modifier","function","alphanumeric","adjustment"],
+        "ans":"modifire"
+    }
+}
+
+for (let key in questionJson){
+        console.log(questionJson[key]["Q"])
+        console.log(questionJson[key]["option"])
+        console.log(questionJson[key]["ans"])
+}
