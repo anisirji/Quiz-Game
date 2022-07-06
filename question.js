cdbc2c125b93bdff88e00e29b05eafe401d6d55a
anisirji <www.allinoneforyouhere@gmail.com>
2022-07-06 10:01:01 +0530

    Auto ui generation compleated

---
+++ question.js
@@ -0,0 +1,34 @@
+
+
+const questionJson = {
+    1:{
+        "Q":" What is “Trend Micro”?",
+        "option":["It is anti-virus software","It is just a program","It is virus program","None of the above"],
+        "ans":"It is anti-virus software"
+    },
+    2:{
+        "Q":"What is the name of the viruses that fool a user into downloading and/or executing them by pretending to be useful applications?",
+        "option":["Cracker","Worm","Trojan horses","Keylogger"],
+        "ans":"Trojan horses"
+    },
+    3:{
+        "Q":"The virus that spread in application software is called as_________",
+        "option":["Boot virus","Macro virus","File virus","Anti virus"],
+        "ans":"Macro virus"
+    },
+    4:{
+        "Q":"What is the virus that spread in computer?",
+        "option":[" It is hardware","It is system software","It is a computer program","It is a windows too"],
+        "ans":"It is a computer program"
+    },
+    5:{
+        "Q":"What is anti-virus?",
+        "option":["It is a computer","It is a program code","It is a company name","It is an application"],
+        "ans":"It is a program code"
+    },
+    6:{
+        "Q":"What is anti-virus?",
+        "option":["It is a computer","It is a program code","It is a company name","It is an application"],
+        "ans":"It is a program code"
+    }
+}
--- script.js
+++ script.js
@@ -8,33 +8,46 @@
 
 const main = document.getElementById("root")
 
+const mainContainer = document.createElement("div");
+mainContainer.className = "mainContainer";
+main.appendChild(mainContainer);
+mainContainer.appendChild(document.createElement("h1").appendChild(document.createTextNode("Quiz")))   
 
-
-const questionJson = {
-    1:{
-        "Q":"MS-Word is an example of _____",
-        "option":["An operating system","A processing device","Application software","An input device"],
-        "ans":"Application software"
-    },
-    2:{
-        "Q":"Ctrl, Shift and Alt are called .......... keys.",
-        "option":["modifier","function","alphanumeric","adjustment"],
-        "ans":"modifire"
-    }
-}
 
 for (let key in questionJson){
         console.log(questionJson[key]["Q"])
         console.log(questionJson[key]["option"])
         console.log(questionJson[key]["ans"])
+
+        createEle(key,questionJson[key]["Q"],questionJson[key]["option"],questionJson[key]["ans"])
 }
 
 
-function createEle(Q,option,ans) {
-    const mainContainer = document.createElement("div");
-    mainContainer.className = "mainContainer";
-    main.appendChild(mainContainer);
 
-    mainContainer.appendChild(document.createElement("h1").appendChild(document.createTextNode("Quiz")))
+function createEle(key,Q,option,ans) {
     
+    const container = document.createElement('div');
+    container.className = "container";
+    mainContainer.appendChild(container);
+
+    const Qp = document.createElement('p')
+    Qp.appendChild(document.createElement('b').appendChild(document.createTextNode(`Question ${key}) `)))
+    Qp.appendChild(document.createElement('span').appendChild(document.createTextNode(Q)))
+    container.appendChild(Qp)
+
+    const optionContainer = document.createElement('div')
+    optionContainer.className="optionContainer"
+    const optionList = document.createElement('ul')
+    for (let options in option){
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(option[options]))
+        optionList.appendChild(li)
+    }
+    optionContainer.appendChild(optionList)
+    container.appendChild(optionContainer);
+
+    const btn = document.createElement('div')
+    btn.className = "button"
+    btn.appendChild(document.createTextNode("Submit"))
+    container.appendChild(btn)
 }
